e65a19a20b4ea3d61f0749ae54ef3c9b45004e0b
Vertigeux <email@email.email>
2019-11-24 19:50:35 +0000

    fixed script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,8 @@
 sudo systemctl stop jifbot.service
-mkdir backup
+mkdir -p backup
 cp ../bin/Debug/netcoreapp3.0/references/* backup/
 cd ..
 git pull
 dotnet build 
-cp scripts/backup/* ../bin/Debug/netcoreapp3.0/references/
+cp scripts/backup/* bin/Debug/netcoreapp3.0/references/
 sudo systemctl start jifbot.service
