aa9f30811f81ed758d4914c6f95134808ad71d42
Vertigeux <email@email.email>
2019-11-24 19:38:15 +0000

    Added a build script

---
+++ scripts/update.sh
@@ -0,0 +1,8 @@
+sudo systemctl stop jifbot.service
+mkdir backup
+cp ../bin/Debug/netcoreapp3.0/references/* backup/
+cd ..
+git pull
+dotnet build 
+cp scripts/backup/* ../bin/Debug/netcoreapp3.0/references/
+sudo systemctl start jifbot.service
