f9377ea1c92618b49a734d25ed355b3dfe9c37ae
Vertigeux <jiffyeljefe@gmail.com>
2021-03-19 16:00:55 -0500

    Group change log items by date

--- Site/javascript/changelog.js
+++ Site/javascript/changelog.js
@@ -1,17 +1,26 @@
 function loadChanges(){
     var json = JSON.parse(JSON.stringify(jifBotChangelog));
-	
+	let logs = new Map();
     for(var i = 0; i < json.length; i++){
-		var container = document.createElement('div')
-		container.id = "change"
-		var date = document.createElement('h3')
-		var change = document.createElement('p')
-		date.innerHTML = json[i].date
-		change.innerHTML = json[i].change
-		container.appendChild(date)
-		container.appendChild(change)
-		document.getElementById("changelog").appendChild(container)
+		if (logs.has(json[i].date)){
+			logs.set(json[i].date, logs.get(json[i].date) + "<br>" + json[i].change);
+		}
+		else{
+			logs.set(json[i].date, json[i].change)
+		}
     }
+	
+	logs.forEach(function(value, key){
+		var container = document.createElement('div');
+		container.id = "change";
+		var date = document.createElement('h3');
+		var change = document.createElement('p');
+		date.innerHTML = key;
+		change.innerHTML = value;
+		container.appendChild(date);
+		container.appendChild(change);
+		document.getElementById("changelog").appendChild(container);
+	});
 }
 
 window.onload = function() {
