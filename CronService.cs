7e5c04729544989aa5f62646fec5cb2111f0d7d3
Vertigeux <jiffyeljefe@gmail.com>
2025-08-16 15:53:09 -0400

    Added QOTD error handling and shows current pool

--- CronService.cs
+++ CronService.cs
@@ -27,48 +27,60 @@ namespace JifBot
                 // Wait 30s before running it back
                 await Task.Delay(30000);
                 var now = DateTime.Now;
-                // Qotd time!!
-                if (now.Hour == 9 && now.DayOfWeek != lastQotdDay)
+
+                try
                 {
-                    Console.WriteLine($"{DateTime.Now} [{"Cron",8}] QOTD: Posting.");
-                    var db = new BotBaseContext();
-                    var servers = db.ServerConfig.AsQueryable().Where(s => s.QotdThreadId != 0).ToList();
-
-                    foreach(var server in servers)
+                    // Qotd time!!
+                    if (now.Hour == 9 && now.DayOfWeek != lastQotdDay)
                     {
-                        var questions = db.Qotd.AsQueryable().Where(q => q.ServerId == server.ServerId && q.AskTimestamp == 0).ToList();
-                        if (questions.Count() > 0)
+                        lastQotdDay = now.DayOfWeek;
+                        var db = new BotBaseContext();
+                        var servers = db.ServerConfig.AsQueryable().Where(s => s.QotdThreadId != 0).ToList();
+
+                        foreach (var server in servers)
                         {
-                            Random random = new Random();
-                            var index = random.Next(questions.Count());
-                            var question = questions[index];
-
-                            var channel = client.GetGuild(server.ServerId).GetChannel(server.QotdForumId) as SocketForumChannel;
-                            var thread = client.GetGuild(server.ServerId).GetThreadChannel(server.QotdThreadId);
-                            var post = await thread.GetMessageAsync(thread.Id) as IUserMessage;
-                            var user = client.GetUser(question.UserId);
-
-                            if (question.Image != null)
+                            var questions = db.Qotd.AsQueryable().Where(q => q.ServerId == server.ServerId && q.AskTimestamp == 0).ToList();
+                            if (questions.Count() > 0)
                             {
-                                var ms = new MemoryStream(question.Image);
+                                var guild = client.GetGuild(server.ServerId);
+                                Console.WriteLine($"{DateTime.Now} [{"Info",8}] QOTD: Posting in {guild.Name}");
 
-                                var image = new Discord.FileAttachment(ms, $"image.{question.ImageType}");
-                                await channel.CreatePostWithFileAsync($"{now.Month}/{now.Day}/{now.Year}", image, text: $"# {question.Question}\nSubmitted by: {user.Mention}");
-                            }
-                            else
-                            {
-                                await channel.CreatePostAsync($"{now.Month}/{now.Day}/{now.Year}", text: $"# {question.Question}\nSubmitted by: {user.Mention}");
-                            }
+                                Random random = new Random();
+                                var index = random.Next(questions.Count());
+                                var question = questions[index];
 
-                            question.AskTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-                            db.SaveChanges();
-                            var embed = new JifBotEmbedBuilder();
-                            embed.PopulateAsQotd(server.ServerId);
-                            await post.ModifyAsync(msg => msg.Embed = embed.Build());
+                                var channel = guild.GetChannel(server.QotdForumId) as SocketForumChannel;
+                                var thread = guild.GetThreadChannel(server.QotdThreadId);
+                                var post = await thread.GetMessageAsync(thread.Id) as IUserMessage;
+                                var user = client.GetUser(question.UserId);
+
+                                if (question.Image != null)
+                                {
+                                    var ms = new MemoryStream(question.Image);
+
+                                    var image = new Discord.FileAttachment(ms, $"image.{question.ImageType}");
+                                    await channel.CreatePostWithFileAsync($"{now.Month}/{now.Day}/{now.Year}", image, text: $"# {question.Question}\nSubmitted by: {user.Mention}");
+                                }
+                                else
+                                {
+                                    await channel.CreatePostAsync($"{now.Month}/{now.Day}/{now.Year}", text: $"# {question.Question}\nSubmitted by: {user.Mention}");
+                                }
+
+                                question.AskTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+                                db.SaveChanges();
+                                var embed = new JifBotEmbedBuilder();
+                                embed.PopulateAsQotd(server.ServerId);
+                                await post.ModifyAsync(msg => msg.Embed = embed.Build());
+                            }
                         }
                     }
-
-                    lastQotdDay = now.DayOfWeek;
+                }
+                catch (Exception ex)
+                {
+                    var cc = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{DateTime.Now} [{"Error",8}] {ex.Message}");
+                    Console.ForegroundColor = cc;
                 }
             }
         }
--- JifBotEmbedBuilder.cs
+++ JifBotEmbedBuilder.cs
@@ -78,13 +78,29 @@ namespace JifBot
         {
             var db = new BotBaseContext();
 
-            var availCount = db.Qotd.AsQueryable().Where(q => q.ServerId == guildId && q.AskTimestamp == 0).Count();
+            var pool = db.Qotd.AsQueryable().Where(q => q.ServerId == guildId && q.AskTimestamp == 0).ToList();
             var postCount = db.Qotd.AsQueryable().Where(q => q.ServerId == guildId && q.AskTimestamp != 0).Count();
 
             Title = "Submit your own QOTD!";
             Description = "Questions will go into a pool, and one will be randomly selected every morning to be the QOTD. To submit a question, use /submitqotd, or use the button below. [Coming Soon]\n\nDo your part to make sure the pool doesn't run dry!";
-            AddField("Current Pool", availCount, inline: true);
+
+            var questions = "[empty]";
+            if (pool.Count > 0)
+            {
+                questions = "";
+                foreach (var question in pool)
+                {
+                    var line = $"- ||{question.Question}||\n";
+                    if (line.Length + questions.Length < 1024)
+                    {
+                        questions += line;
+                    }
+                }
+            }
+            
             AddField("Questions Posted", postCount, inline: true);
+            AddField("Pool Size", pool.Count(), inline: true);
+            AddField("Current Pool", questions);
             ThumbnailUrl = "https://cdn.discordapp.com/emojis/571859749860278293.png";
         }
     }
